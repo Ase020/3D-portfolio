7f0eeca514e8224b53b3eff88f219e240db8b27e
felixNyalenda <felixasejunior@gmail.com>
2023-05-11 13:05:17 +0300

    hero section build

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,24 +11,45 @@ const Section = styled.section`
 `;
 
 const Container = styled.div`
-  height: 100vh;
+  height: 90vh;
   width: 1200px;
   display: flex;
   justify-content: space-between;
-  background: #b5b3b3;
 `;
 
-const Left = styled.div``;
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 10px;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 54px;
+  margin: 12px 0;
+`;
 
-const Intro = styled.div``;
+const Intro = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
 
-const Line = styled.img``;
+const Line = styled.img`
+  height: 5px;
+`;
 
-const Subtitle = styled.p``;
+const Subtitle = styled.h2`
+  color: #da4ea2;
+  font-size: 20px;
+  margin: 6px 0;
+`;
 
-const Desc = styled.p``;
+const Desc = styled.p`
+  color: lightgray;
+  font-size: 18px;
+`;
 
 const Button = styled.button`
   background: #da4ea2;
@@ -37,11 +58,29 @@ const Button = styled.button`
   padding: 10px;
   border-radius: 4px;
   cursor: pointer;
+  width: 100px;
 `;
 
-const Right = styled.div``;
+const Right = styled.div`
+  flex: 3;
+  position: relative;
+`;
 
-const Img = styled.img``;
+const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  inset: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @keyframes animate {
+    100% {
+      transform: translateY(20px);
+    }
+  }
+`;
 
 const Hero = () => {
   return (
